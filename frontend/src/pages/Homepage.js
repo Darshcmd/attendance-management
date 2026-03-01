6231476610e8662528ef9e5d334b2fda2c7db0f2
Darsh <soni.darsh2004@gmail.com>
2026-03-01 21:49:13 +0530

    Fix: Update image import from students.svg to vit.jpeg

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Grid, Box, Button } from '@mui/material';
 import styled from 'styled-components';
-import Students from "../assets/students.svg";
+import Students from "../assets/vit.jpeg";
 import { LightPurpleButton } from '../components/buttonStyles';
 import ProjectCredits from '../components/ProjectCredits';
 
